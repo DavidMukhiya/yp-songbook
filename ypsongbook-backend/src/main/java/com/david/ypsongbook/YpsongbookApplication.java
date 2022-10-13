948f27ec2dee192080d344b37696cb95feb7f32d
David Prakash Mukhiya <sherlock.41v94@gmail.com>
2022-10-12 23:59:16 -0700

    added frontend and backend intial project file

---
+++ ypsongbook-backend/src/main/java/com/david/ypsongbook/YpsongbookApplication.java
@@ -0,0 +1,13 @@
+package com.david.ypsongbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YpsongbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YpsongbookApplication.class, args);
+	}
+
+}
